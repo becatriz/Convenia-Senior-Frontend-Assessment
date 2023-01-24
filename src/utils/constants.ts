b4ebaa2cd9dd889660d20c10841061c59f7d26e6
Rebeca <becatriz7@gmail.com>
2023-01-24 13:16:56 -0400

    feat: ✨ create constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,11 +1,36 @@
-import type { WeekDay } from "~/types/weekday";
+import type { MapItems } from "@/types/command";
 
-export const WEEKDAY: WeekDay = {
-  0: "Domingo",
-  1: "Segunda-feira",
-  2: "Terça-feira",
-  3: "Quarta-feira",
-  4: "Quinta-feira",
-  5: "Sexta-feira",
-  6: "Sabádo",
+export const WEEKDAY: Array<string> = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sabádo",
+];
+
+export const TYPE_FOOD: MapItems = {
+  drinks: "Bebidas",
+  foods: "Lanches",
+  servings: "Porções",
+  salads: "Saladas",
 };
+
+export const OPTIONS_SELECTED_CURRENCY: Array<{
+  value: string;
+  option: string;
+}> = [
+  {
+    value: "BRL",
+    option: "🇧🇷 R$ BRL 💵",
+  },
+  {
+    value: "EUR",
+    option: "🇪🇺 £ EUR 💶",
+  },
+  {
+    value: "USD",
+    option: "🇺🇸 $ USD 💴",
+  },
+];
