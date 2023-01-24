6dc6e3c8d00b890c57a038ae70098e8314fbefd2
Rebeca <becatriz7@gmail.com>
2023-01-24 12:36:15 -0400

    feat: ✨ create CardUI component

---
+++ src/components/components-ui/CardUI.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps({
+  title: {
+    default: "",
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <div class="container">
+    <div class="card">
+      <p class="card__title">{{ props.title }}</p>
+      <slot name="body" />
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
