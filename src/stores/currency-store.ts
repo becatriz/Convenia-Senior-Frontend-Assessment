8e4fe7ecbaaf72341bbb4de0b3b514036e279706
Rebeca <becatriz7@gmail.com>
2023-01-24 13:09:59 -0400

    feat: ✨ create currency store

---
+++ src/stores/currency-store.ts
@@ -0,0 +1,55 @@
+import { defineStore } from "pinia";
+import type { CurrencyCurrent } from "@/types/currency";
+import type { CurrencyConversion } from "@/graphql/types/generated";
+
+interface ICurrency {
+  baseCurrency: CurrencyConversion | undefined | null;
+  chosenOption: string;
+}
+
+export const useCurrencyStore = defineStore("currency-store", {
+  state: (): ICurrency => ({
+    baseCurrency: {},
+    chosenOption: "BRL",
+  }),
+
+  actions: {
+    setBaseCurrencyInfo(item: CurrencyConversion | undefined | null) {
+      this.baseCurrency = item;
+    },
+
+    setChosenCurrency(item: string) {
+      this.chosenOption = item;
+    },
+  },
+  getters: {
+    getCurrencyInfo: (state) => state.baseCurrency,
+    getChosenOption: (state) => state.chosenOption,
+    getCurrencyChosen: (state): CurrencyCurrent => {
+      const isCurrencyDefault =
+        state.baseCurrency?.baseCurrencyInfo?.code === state.chosenOption;
+
+      if (isCurrencyDefault) {
+        return {
+          symbol: state.baseCurrency?.baseCurrencyInfo?.symbol || "",
+          rate: 1,
+        };
+      }
+
+      return state.baseCurrency?.conversions?.reduce((acc, item) => {
+        if (item?.currencyInfo?.code === state.chosenOption) {
+          return {
+            ...acc,
+            symbol: item?.currencyInfo.symbol,
+            rate: item?.rate,
+          };
+        }
+        return acc;
+      }, {}) as CurrencyCurrent;
+    },
+  },
+
+  persist: {
+    enabled: true,
+  },
+});
