72833aa9716d4a730c96cdda7da1047cf575bb93
Rebeca <becatriz7@gmail.com>
2023-01-24 13:19:06 -0400

    feat: ✨ add files storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
-import "../src/assets/main.scss"
+import "../src/assets/main.scss";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -8,4 +8,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
