15c38acb9b644852cc0cdadbf53b6c62813029f8
Rebeca <becatriz7@gmail.com>
2023-01-24 12:24:43 -0400

    feat: ✨ create OrderView page

--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -1,55 +1,214 @@
 <script setup lang="ts">
-import ButtonGlobal from "@/components/ButtonGlobal.vue";
-import AccordionGlobal from "@/components/AccordionGlobal.vue";
-import TableCollapseGlobal from "@/components/TableCollapseGlobal.vue";
+import { onMounted, ref, toRaw } from "vue";
+import { storeToRefs } from "pinia";
+import { useOrderStore } from "@/stores/order-store";
+import { usePaymentsStore } from "@/stores/payment-store";
+import { useCurrencyStore } from "@/stores/currency-store";
+
+import ButtonUI from "@/components/components-ui/ButtonUI.vue";
+import AccordionUI from "@/components/components-ui/AccordionUI.vue";
+import TableUI from "@/components/components-ui/TableUI.vue";
+import CardUI from "@/components/components-ui/CardUI.vue";
+import TotalAccount from "@/components/TotalAccount.vue";
+import InputMinPlusUI from "@/components/components-ui/InputMinPlusUI.vue";
+
+import { useToast } from "vue-toastification";
+import { useRoute } from "vue-router";
+
+import type { Command, Items, MapItems } from "@/types/command";
+import { TYPE_FOOD } from "@/utils/constants";
+import { getFormattedValue } from "@/utils/formattedValue";
+import LoadingUI from "@/components/components-ui/LoadingUI.vue";
+
+const toast = useToast();
+const route = useRoute();
+const storeOrders = useOrderStore();
+const storeCurrency = useCurrencyStore();
+const { getCommand, getTotalAccount, getOrders } = storeToRefs(storeOrders);
+const { getCurrencyChosen } = storeToRefs(storeCurrency);
+const { setCommand, updateOrder } = useOrderStore();
+const { createPayments } = usePaymentsStore();
+
+const paramsID = ref("");
+const isLoading = ref(false);
+const clonedOrder = ref<Array<Command>>([]);
 
 function handleGoBack() {
   history.back();
 }
+
+function handleAddItem({ label, item }: { label: string; item: string }) {
+  clonedOrder.value = structuredClone(toRaw(getCommand.value));
+
+  clonedOrder.value.forEach((value) => {
+    const items = value[label as keyof MapItems];
+
+    items.forEach((val) => {
+      if (val.name === item) {
+        val.amount++;
+      }
+    });
+  });
+
+  setCommand(clonedOrder.value);
+  getTotalAccount;
+}
+
+function handleRemoveItem({ label, item }: { label: string; item: string }) {
+  clonedOrder.value = structuredClone(toRaw(getCommand.value));
+
+  clonedOrder.value.forEach((value) => {
+    const items = value[label as keyof MapItems];
+
+    items.forEach((val) => {
+      if (val.name === item) {
+        val.amount--;
+      }
+    });
+  });
+  setCommand(clonedOrder.value);
+  getTotalAccount;
+}
+
+async function handleConfirmedUpdate(): Promise<void> {
+  setNewValuesForOrders();
+
+  handleGoBack();
+
+  if (clonedOrder.value.length) {
+    await updateOrder(getPayload());
+    toast.success("Comanda atualizada com sucesso!");
+  }
+}
+
+function setNewValuesForOrders(): void {
+  getOrders.value.forEach((value) => {
+    if (value.code === paramsID.value) {
+      if (value.status === "free") {
+        value.status = "active";
+      }
+      value.command = clonedOrder.value;
+    }
+  });
+}
+
+function getPayload(): Array<Items> {
+  return getOrders.value.filter((value) => value.code === paramsID.value);
+}
+
+function setIsLoading(status: boolean) {
+  isLoading.value = status;
+}
+
+onMounted(async () => {
+  setIsLoading(true);
+
+  document.body.style.backgroundColor = "	#808080";
+
+  paramsID.value = route.params["id"] as string;
+
+  getOrders.value.forEach(async (value) => {
+    if (value.code === paramsID.value) {
+      if (value.status == "closed") {
+        value.status = "free";
+        await createPayments({
+          id: paramsID.value,
+          total: 0,
+          payments: [],
+        });
+      }
+    }
+  });
+  await updateOrder(getPayload());
+
+  setIsLoading(false);
+});
 </script>
 
 <template>
-  <div class="card">
-    <p class="order__title">Novo Pedido</p>
-    <AccordionGlobal v-for="(item, index) in [1, 2]" :key="index">
-      <template #list>
-        <TableCollapseGlobal />
-      </template>
-    </AccordionGlobal>
-    <footer>
-      <div class="order__total">
-        <p>Mesa 1</p>
-        <p>Total: R$ 32,00</p>
+  <LoadingUI :is-loading="isLoading" />
+  <CardUI title="Novo Pedido">
+    <template #body>
+      <div v-for="(item, index) in getCommand" :key="index">
+        <AccordionUI
+          v-for="label in Object.keys(item)"
+          :key="label"
+          :title="TYPE_FOOD[label as keyof MapItems]"
+        >
+          <template #list>
+            <div
+              v-for="(line, indexLine) in item[label as keyof MapItems]"
+              :key="indexLine"
+            >
+              <TableUI
+                :title="line.name"
+                :value="`${
+                  getCurrencyChosen?.symbol || 'R$'
+                } ${getFormattedValue(line.value, getCurrencyChosen?.rate)}`"
+              >
+                <template #component>
+                  <InputMinPlusUI
+                    :amount="line.amount"
+                    @handle-add-item="
+                      handleAddItem({
+                        label,
+                        item: line.name,
+                      })
+                    "
+                    @handle-remove-item="
+                      handleRemoveItem({
+                        label,
+                        item: line.name,
+                      })
+                    "
+                  />
+                </template>
+              </TableUI>
+            </div>
+          </template>
+        </AccordionUI>
       </div>
-      <div class="grid">
-        <div class="row">
-          <div class="col s12 m6">
-            <ButtonGlobal
-              @handle-click="handleGoBack"
-              variant="tertiary"
-              label="Cancelar"
-            />
-          </div>
-          <div class="col s12 m6">
-            <ButtonGlobal label="Confirmar" />
+    </template>
+    <template #footer>
+      <footer>
+        <TotalAccount
+          :title="`Mesa ${paramsID}`"
+          :total="getTotalAccount.total"
+          :symbol="getCurrencyChosen?.symbol || 'R$'"
+        />
+        <div class="footer-btn">
+          <div class="grid">
+            <div class="row center">
+              <div class="col s12 m5">
+                <ButtonUI
+                  variant="tertiary"
+                  label="Cancelar"
+                  @handle-click="handleGoBack"
+                />
+              </div>
+              <div class="col s12 m5">
+                <ButtonUI
+                  label="Confirmar"
+                  @handle-click="handleConfirmedUpdate"
+                />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </footer>
-  </div>
+      </footer>
+    </template>
+  </CardUI>
 </template>
 
 <style lang="scss" scoped>
 @import "../assets/DS/variables", "../assets/DS/typography",
   "../assets/DS/colors";
 
-
 .order {
   &__title {
     text-align: center;
     @extend %heading-os-18-600;
   }
-
   &__total {
     display: flex;
     justify-content: space-between;
@@ -58,7 +217,6 @@ function handleGoBack() {
 }
 
 footer {
-  width: 86%;
   margin: 0 auto;
 
   @media #{$small-and-down} {
