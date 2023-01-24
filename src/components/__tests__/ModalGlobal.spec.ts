e492e387d7866a74836d007e0fe5bc6d6f85d075
Rebeca <becatriz7@gmail.com>
2023-01-24 12:55:52 -0400

    test: ✅ create test for ModalUI component

--- src/components/__tests__/ModalGlobal.spec.ts
+++ src/components/__tests__/ModalGlobal.spec.ts
@@ -1,48 +1,48 @@
 import { describe, it, expect } from "vitest";
 
 import { mount } from "@vue/test-utils";
-import ModalGlobal from "../ModalGlobal.vue";
+import ModalUI from "../../components/components-ui/ModalUI.vue";
 
-describe("ModalGlobal", () => {
-  it("Renders properly for isOpenModal", () => {
-    const wrapper = mount(ModalGlobal, { props: { isOpenModal: true } });
+describe("ModalUI", () => {
+  it("Should renders properly for isOpenModal", () => {
+    const wrapper = mount(ModalUI, { props: { isOpenModal: true } });
     expect(wrapper.find(".modal").exists()).toBe(true);
   });
 
-  it("Renders no properly for isOpenModal", () => {
-    const wrapper = mount(ModalGlobal, { props: { isOpenModal: false } });
+  it("Should renders no properly for isOpenModal", () => {
+    const wrapper = mount(ModalUI, { props: { isOpenModal: false } });
     expect(wrapper.find(".modal").exists()).toBe(false);
   });
 
-  it("Renders no properly for isCanCloseModal", () => {
-    const wrapper = mount(ModalGlobal, {
+  it("Should renders no properly for isCanCloseModal", () => {
+    const wrapper = mount(ModalUI, {
       props: { isOpenModal: true, isCanCloseModal: false },
     });
     expect(wrapper.find("span").exists()).toBe(false);
   });
 
-  it("Renders properly for isCanCloseModal", () => {
-    const wrapper = mount(ModalGlobal, {
+  it("Should renders properly for isCanCloseModal", () => {
+    const wrapper = mount(ModalUI, {
       props: { isOpenModal: true, isCanCloseModal: true },
     });
     expect(wrapper.find("span").exists()).toBe(true);
   });
 
-  it("Emit event input:modelValue", () => {
-    const wrapper = mount(ModalGlobal);
-    wrapper.vm.$emit('input:modelValue');
-    expect(wrapper.emitted('input:modelValue')).toBeTruthy();
+  it("Should emit event input:modelValue", () => {
+    const wrapper = mount(ModalUI);
+    wrapper.vm.$emit("input:modelValue");
+    expect(wrapper.emitted("input:modelValue")).toBeTruthy();
   });
 
   it("Render slot Footer", () => {
-    const wrapper = mount(ModalGlobal, {
-      props:{
-        isOpenModal: true
+    const wrapper = mount(ModalUI, {
+      props: {
+        isOpenModal: true,
       },
-      slots:{
-        footer: '<div>Footer</div>'
-      }
+      slots: {
+        footer: "<div>Footer</div>",
+      },
     });
-    expect(wrapper.html()).toContain('<div>Footer</div>')
+    expect(wrapper.html()).toContain("<div>Footer</div>");
   });
 });
