48f5901b31ec075ce8d77f47f55404700577167f
Rebeca <becatriz7@gmail.com>
2023-01-24 12:23:53 -0400

    feat: ✨ create HomeView page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,83 @@
 <script setup lang="ts">
+import { ref, onMounted } from "vue";
+import type { GetCurrencyConversionQuery } from "@/graphql/types/generated";
+
+import gql from "graphql-tag";
+import { useQuery } from "@vue/apollo-composable";
+
 import { storeToRefs } from "pinia";
 import { usePersonStore } from "@/stores/person-store";
-import { ref } from "vue";
-import CircleGlobal from "@/components/CircleGlobal.vue";
-import ModalGlobal from "@/components/ModalGlobal.vue";
-import ButtonGlobal from "@/components/ButtonGlobal.vue";
+import { useOrderStore } from "@/stores/order-store";
+import { usePaymentsStore } from "@/stores/payment-store";
+import { useCurrencyStore } from "@/stores/currency-store";
+
+import CircleAction from "@/components/CircleAction.vue";
+import ModalUI from "@/components/components-ui/ModalUI.vue";
+import ButtonUI from "@/components/components-ui/ButtonUI.vue";
+import SelectUI from "@/components/components-ui/SelectUI.vue";
+import LoadingUI from "@/components/components-ui/LoadingUI.vue";
+
 import router from "@/router";
 
+import { OPTIONS_SELECTED_CURRENCY } from "@/utils/constants";
+
+import { useToast } from "vue-toastification";
+
+const toast = useToast();
+const storeOrders = useOrderStore();
 const storePerson = usePersonStore();
+const storeCurrency = useCurrencyStore();
+
+const { getCurrencyChosen, getChosenOption } = storeToRefs(storeCurrency);
 const { getPerson } = storeToRefs(storePerson);
+const { getOrders, getTotalAccount, getOrderTotalConfirmed } =
+  storeToRefs(storeOrders);
+
 const { setPerson } = usePersonStore();
+const { fetchPayments } = usePaymentsStore();
+const { fetchOrders, setCodeTable, setConfirmedOrder, setTotalPartial } =
+  useOrderStore();
+const { setBaseCurrencyInfo, setChosenCurrency } = useCurrencyStore();
 
 const personName = ref("");
 const isOpenModal = ref(true);
 const minNameValue = ref(3);
+const isLoading = ref(false);
+const currencyOptionsConversion = ref(OPTIONS_SELECTED_CURRENCY);
+
+const variables = ref({
+  baseCurrency: "BRL",
+  convertCurrencies: ["USD", "EUR"],
+});
+
+const { onResult, onError, result, loading } =
+  useQuery<GetCurrencyConversionQuery>(
+    gql`
+      query currencyConversion(
+        $baseCurrency: String!
+        $convertCurrencies: [String]!
+      ) {
+        currencyConversion(
+          baseCurrency: $baseCurrency
+          convertCurrencies: $convertCurrencies
+        ) {
+          baseCurrencyInfo {
+            code
+            symbol
+          }
+          conversions {
+            currencyInfo {
+              code
+              symbol
+            }
+            rate
+            timestamp
+          }
+        }
+      }
+    `,
+    variables,
+  );
 
 function setNameValue(value: string): void {
   personName.value = value;
@@ -27,40 +91,124 @@ function handleClick() {
   isOpenModal.value = false;
 }
 
-function handleUpdateCommand(): void {
+function handleUpdateCommand(codeTable: string): void {
+  setCodeTable(codeTable);
+
   router.push({
     name: "order",
     params: {
-      id: "1",
+      id: codeTable,
     },
   });
 }
 
-function handleCloseCommand(): void {
+function handleCloseCommand(codeTable: string): void {
+  setCodeTable(codeTable);
+
   router.push({
     name: "payments",
     params: {
-      id: "1",
+      id: codeTable,
     },
   });
 }
+
+function handleChoseCurrency(value: string) {
+  setChosenCurrency(value);
+
+  setStatusIsLoading(true);
+
+  getOrders.value.forEach((item) => {
+    setCodeTable(item.code);
+    setTotalPartial(getTotalAccount.value);
+    setConfirmedOrder(getTotalAccount.value);
+  });
+
+  setStatusIsLoading(false);
+}
+
+async function getOrdersAndPayments() {
+  setStatusIsLoading(true);
+
+  await fetchPayments();
+  await fetchOrders();
+
+  getOrders.value.forEach((item) => {
+    setCodeTable(item.code);
+    setTotalPartial(getTotalAccount.value);
+    setConfirmedOrder(getTotalAccount.value);
+  });
+
+  setStatusIsLoading(false);
+}
+
+function setStatusIsLoading(status: boolean) {
+  isLoading.value = status;
+}
+
+onResult(async ({ data }) => {
+  setStatusIsLoading(loading.value);
+
+  setBaseCurrencyInfo(data.currencyConversion);
+
+  await getOrdersAndPayments();
+});
+
+onError(async (data) => {
+  setStatusIsLoading(loading.value);
+
+  await getOrdersAndPayments();
+  setBaseCurrencyInfo(null);
+  setTotalPartial(getTotalAccount.value);
+  if (data.name === "ApolloError") {
+    toast.error(
+      "Erro ao buscar listagem para conversão de moedas. Inicialize API para conectar.",
+    );
+    setStatusIsLoading(loading.value);
+    return;
+  }
+  setStatusIsLoading(loading.value);
+  toast.error("Ocorreu um erro para buscar listagem de moedas");
+});
+
+onMounted(() => {
+  document.body.style.backgroundColor = "#FFFF";
+});
 </script>
 <template>
   <main>
-   <div v-if="getPerson.name" class="content-table">
-    <CircleGlobal @update:command="handleUpdateCommand"  @close:command="handleCloseCommand"/>
-   </div>
-    <ModalGlobal
-      @input:modelValue="setNameValue"
+    <LoadingUI v-if="getPerson.name" :is-loading="isLoading" />
+    <div v-if="getPerson.name" class="content-table">
+      <div class="grid">
+        <div class="row end">
+          <SelectUI
+            v-if="result"
+            class="col s12"
+            :items="currencyOptionsConversion"
+            :default="getChosenOption"
+            @onchange="handleChoseCurrency"
+          />
+        </div>
+      </div>
+      <CircleAction
+        :items="getOrders"
+        :account="getOrderTotalConfirmed"
+        :currency="getCurrencyChosen"
+        @update:command="handleUpdateCommand"
+        @close:command="handleCloseCommand"
+      />
+    </div>
+    <ModalUI
       :is-open-modal="!getPerson.name && isOpenModal"
+      @input:model-value="setNameValue"
     >
       <template #footer>
-        <ButtonGlobal
-          @handleClick="handleClick"
+        <ButtonUI
           :disabled="personName.length < minNameValue"
+          @handle-click="handleClick"
         />
       </template>
-    </ModalGlobal>
+    </ModalUI>
   </main>
 </template>
 
@@ -73,7 +221,14 @@ div {
   color: color("neutral", "white");
 }
 
-.content-table{
+.content-table {
   margin-top: 100px;
+
+  &__selected-ui {
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 }
 </style>
