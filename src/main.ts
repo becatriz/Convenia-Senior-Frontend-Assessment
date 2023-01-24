a1debdff39879e9c33d8115207477bb313a74305
Rebeca <becatriz7@gmail.com>
2023-01-24 13:18:28 -0400

    feat: ✨ add configs store, mirage, appolo client

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,44 @@
-import { createApp } from "vue";
+import { createApp, provide, h } from "vue";
 import { createPinia } from "pinia";
+import type { PluginOptions } from "vue-toastification";
+import piniaPersist from "pinia-plugin-persist";
+import Toast, { POSITION } from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
 import App from "./App.vue";
 import router from "./router";
+import { makeServer } from "@/plugins/mirage";
+import { DefaultApolloClient } from "@vue/apollo-composable";
+import { ApolloClient, InMemoryCache } from "@apollo/client/core";
 
 import "./assets/main.scss";
 
-const app = createApp(App);
+if (import.meta.env.MODE === "development") {
+  makeServer();
+}
 
-app.use(createPinia());
+const cache = new InMemoryCache();
+
+const apolloClient = new ApolloClient({
+  cache,
+  uri: "http://localhost:3000/graphql",
+});
+
+const pinia = createPinia();
+pinia.use(piniaPersist);
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient);
+  },
+
+  render: () => h(App),
+}).use(pinia);
+
+const options: PluginOptions = {
+  position: POSITION.TOP_RIGHT,
+};
+
 app.use(router);
-
+app.use(Toast, options);
 app.mount("#app");
