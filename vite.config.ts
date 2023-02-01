27a4a3323312d2b147c77b9041f38f00f72185aa
Rebeca <becatriz7@gmail.com>
2023-02-01 10:20:23 -0400

    feat: ✨ add workflow actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
+import type { UserConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import type { InlineConfig } from "vitest";
+
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    coverage: {
+      reporter: ["lcov", "html"],
+      exclude: ["**/*.spec.ts"],
+    },
+  },
   server: {
     port: 8080,
   },
@@ -14,4 +26,4 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-});
+}) as VitestConfigExport;
