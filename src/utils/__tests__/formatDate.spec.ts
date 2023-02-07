c73861fb88dd293d6407cc7e355a56858a840a29
Rebeca <becatriz7@gmail.com>
2023-02-06 22:09:18 -0300

    test: ✅ update tests

--- src/utils/__tests__/formatDate.spec.ts
+++ src/utils/__tests__/formatDate.spec.ts
@@ -1,16 +1,36 @@
 import { describe, it, expect } from "vitest";
-import { getFormattedHour, getFormattedDateWithHour } from "../formatDate";
+import {
+  getFormattedHour,
+  getFormattedDateWithHour,
+  _getHours,
+  _getMinutes,
+} from "../formatDate";
 
 describe("Format Date", () => {
   it("Should formatted Hour", () => {
     expect(getFormattedHour(new Date("2023-01-24T01:12:19.836Z"))).equals(
-      "21:12",
+      "22:12",
+    );
+  });
+
+  it("Should formatted Hour undefined", () => {
+    const today = new Date();
+    expect(getFormattedHour(undefined)).equals(
+      `${_getHours(today.getHours())}:${_getMinutes(today.getMinutes())}`,
     );
   });
 
   it("Should formatted Date with hour", () => {
     expect(
       getFormattedDateWithHour(new Date("2023-01-24T01:12:19.836Z")),
-    ).equals("23/01/2023 21:12");
+    ).equals("23/01/2023 22:12");
+  });
+
+  it("Should get Minutes formatted", () => {
+    expect(_getMinutes(1)).equals("01");
+  });
+
+  it("Should get Hour formatted", () => {
+    expect(_getHours(6)).equals("06");
   });
 });
