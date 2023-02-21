cce0fd18cec6325c50aedf4addb12bf0aae7ea3b
Rebeca <becatriz7@gmail.com>
2023-02-21 01:38:45 -0300

    feat: 🤡 add seeds and route for server graphql

--- src/plugins/mirage/modules/index.ts
+++ src/plugins/mirage/modules/index.ts
@@ -3,4 +3,5 @@ import { Model } from "miragejs";
 export default {
   order: Model as typeof Model,
   payment: Model as typeof Model,
+  currencyConversion: Model as typeof Model,
 };
--- src/plugins/mirage/seeds/index.ts
+++ src/plugins/mirage/seeds/index.ts
@@ -1,10 +1,12 @@
 import type { Server } from "miragejs";
 import createOrderSeed from "./order.seed";
 import createPaymentSeed from "./payment.seed";
+import createCurrencySeed from "../graphql/seeds/currency.seed";
 
 function seeds(server: Server) {
   createOrderSeed(server);
   createPaymentSeed(server);
+  createCurrencySeed(server);
 }
 
 export default seeds;
